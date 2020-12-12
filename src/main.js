393f765d28a1cebb2878610e1c4c3b7171aa2571
染秋 <1543013648@qq.com>
2020-12-12 14:49:00 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//加载 Vant 组件库
+import Vant from 'vant'
+import 'amfe-flexible'
+
+import 'vant/lib/index.css'
+// 加载全局样式
+import './styles/index.less'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
